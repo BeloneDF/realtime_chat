a1374dde7097236ef25ef355cbd5e87d55336da6
Belone Dordete Fraga <40274926+BeloneDF@users.noreply.github.com>
2023-04-20 20:32:09 -0300

    put_new_user

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-
+const axios = require("axios");
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-app.post("/auth", (req, res) => {
-  const { email, password } = req.body;
-  return res.json({ email: email, password: password, secret: "sha432..." });
+app.post("/auth", async (req, res) => {
+  const { username} = req.body;
+  const key = "ccdfb912-908b-4483-999b-4184adb1259c";
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": key } }
+    );
+    return res.status(r.status).json(r.data)
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data)
+  }
+
 });
 
 app.listen(3000, () => {
