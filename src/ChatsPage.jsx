4fcd749c474328a39cfa259cdf7caf74f0b5113d
Belone Dordete Fraga <40274926+BeloneDF@users.noreply.github.com>
2023-04-20 21:28:17 -0300

    a

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -1,11 +1,11 @@
 import { PrettyChatWindow } from "react-chat-engine-pretty";
-
+import { variables } from "./variables";
 const ChatsPage = (props) => {
 
   return (
     <div className="background">
       <PrettyChatWindow
-        projectId={import.meta.env.PROJECTID}
+        projectId={variables.projectId}
         username={props.user.username}
         secret={props.user.secret}
       />
