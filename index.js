b0f464327a1c29a0082611ae4bf9a178328be1d8
BeloneDF <belonefraga1@hotmail.com>
2023-04-21 18:39:14 -0300

    hode keys

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const variables = require("./variables");
+
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/auth", async (req, res) => {
-  const { username} = req.body;
-  const key = "ccdfb912-908b-4483-999b-4184adb1259c";
+  const { username } = req.body;
 
   try {
     const r = await axios.put(
-      "https://api.chatengine.io/users/",
+      variables.API_URL,
       { username: username, secret: username, first_name: username },
-      { headers: { "private-key": key } }
+      { headers: { "private-key": variables.CHAT_KEY } }
     );
-    return res.status(r.status).json(r.data)
+    return res.status(r.status).json(r.data);
   } catch (e) {
-    return res.status(e.response.status).json(e.response.data)
+    return res.status(e.response.status).json(e.response.data);
   }
-
 });
 
 app.listen(3000, () => {
---
+++ variables.js
@@ -0,0 +1,5 @@
+module.exports = {
+    API_URL: 'https://api.chatengine.io/users/',
+    CHAT_KEY: 'ccdfb912-908b-4483-999b-4184adb1259c',
+}
+
